5e819bfad6ec3cf5926590be46192e3a10d31e45
Yorso <jorgeguzon@gmail.com>
2016-10-05 21:35:11 +0200

    change

--- src/test/resources/nosql/example.groovy
+++ src/test/resources/nosql/example.groovy
@@ -1,9 +1,1 @@
 @Grab(group='com.gmongo', module='gmongo', version='0.9.3') 
-//import com.gmongo.GMongo 
-
-// Instantiate gmongo.GMongo object 
-// It defaults to localhost, but you can pass connection settings through the constructor 
-//def mongo = new GMongo() 
-
-// Get a reference to the db
-//def db = mongo.getDB("physicianfinder")
