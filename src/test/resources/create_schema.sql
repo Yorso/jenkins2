caa8104f198a76071de785a704b4622039cb7484
Yorso <jorgeguzon@gmail.com>
2016-10-05 12:31:22 +0200

    changes

--- src/test/resources/create_schema.sql
+++ src/test/resources/create_schema.sql
@@ -1,6 +1,6 @@
 <!-- DROP DATABASE IF EXISTS jenkins_test_db; -->
-CREATE SCHEMA jenkins_test_db;
-USE jenkins_test_db;
+CREATE SCHEMA `jenkins_test_db` ;
+USE `jenkins_test_db`;
 CREATE TABLE user(
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(100) NOT NULL,
