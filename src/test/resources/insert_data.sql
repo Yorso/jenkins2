01b0d1c5174b3747bf2ccdc6fdfe344cf14d6488
Yorso <jorgeguzon@gmail.com>
2016-10-05 14:30:45 +0200

    changes

--- src/test/resources/insert_data.sql
+++ src/test/resources/insert_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO `jenkins_test_db`.`user` (`first_name`, `age`) VALUES ('Peter', '34');
-INSERT INTO `jenkins_test_db`.`user` (`first_name`, `age`) VALUES ('John', '54');
+INSERT INTO `jenkins_test_db`.`user` (`first_name`, `age`) VALUES ('Peter Parker', '34');
+INSERT INTO `jenkins_test_db`.`user` (`first_name`, `age`) VALUES ('Homer Simpson', '38');
