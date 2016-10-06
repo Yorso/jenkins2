e7bc5b35da52928d9226be3042e29dec038ca1a8
Yorso <jorgeguzon@gmail.com>
2016-10-06 11:40:19 +0200

    changes

--- src/test/resources/nosql/example.groovy
+++ src/test/resources/nosql/example.groovy
@@ -1,5 +1,5 @@
 // def depFile = new File(project.build.outputDirectory, 'deps.txt')
-def depFile = new File('/home/jorge', 'deps.txt')
+def depFile = new File('/opt/tomcat8/.jenkins', 'deps.txt')
 
 project.dependencies.each() {
   depFile.write("${it.groupId}:${it.artifactId}:${it.version}")
