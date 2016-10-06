669b298e1dbe9ae9876f2e4cd5638e4f95b690b6
Yorso <jorgeguzon@gmail.com>
2016-10-06 15:36:46 +0200

    changes

--- src/test/resources/nosql/mongodb_test.groovy
+++ src/test/resources/nosql/mongodb_test.groovy
@@ -3,6 +3,7 @@ import com.mongodb.DBCollection
 import com.mongodb.DB
 import com.mongodb.BasicDBObject
 
+// Creating all referent to DB
 class MongoService { 
 
     private MongoClient mongoClient 
@@ -53,3 +54,45 @@ coll.insert(data)
 coll.find().toArray().each {
     println it
 }
+
+//Testing
+class Example {
+   static void main(String[] args) {
+      Student mst = new Student();
+      mst.name = "Joe";
+      mst.ID = 1;
+      println(mst.Display())
+   } 
+} 
+ 
+public class Student {
+   String name;
+   int ID;
+	
+   String Display() {
+      return name +ID;
+   }  
+}
+
+class StudentTest extends GroovyTestCase {
+   void testDisplay() {
+      def stud = new Student(name : 'Joe', ID : '1')
+      def expected = 'Joe1'
+      assertToString(stud.Display(), expected)
+   }
+}
+
+import groovy.util.GroovyTestSuite 
+import junit.framework.Test 
+import junit.textui.TestRunner 
+
+class AllTests { 
+   static Test suite() { 
+      def allTests = new GroovyTestSuite() 
+      allTests.addTestSuite(StudentTest.class) 
+      allTests.addTestSuite(EmployeeTest.class) 
+      return allTests 
+   } 
+} 
+
+TestRunner.run(AllTests.suite())
