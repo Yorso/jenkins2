720cb096296879ece357cf8ea5f380c285445a00
Yorso <jorgeguzon@gmail.com>
2016-10-06 14:19:56 +0200

    Groovy files deleted

--- src/test/resources/nosql/example.groovy
+++
@@ -1,8 +0,0 @@
-// Path where 'deps.txt' file is created: project.build.outputDirectory ==> /opt/tomcat8/.jenkins/workspace/jenkins2/target/classes
-def depFile = new File(project.build.outputDirectory, 'deps.txt')
-
-// def depFile = new File('/opt/tomcat8/.jenkins', 'deps.txt')
-
-project.dependencies.each() {
-  depFile.write("${it.groupId}:${it.artifactId}:${it.version}:${project.build.outputDirectory}")
-}
