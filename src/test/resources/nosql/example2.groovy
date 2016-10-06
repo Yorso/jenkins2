2aeb0329d6ad7e5fbdf8121278608c5c62fe47ef
Yorso <jorgeguzon@gmail.com>
2016-10-06 12:34:06 +0200

    changes

--- src/test/resources/nosql/example2.groovy
+++ src/test/resources/nosql/example2.groovy
@@ -1,27 +1,40 @@
-// Grab the jar
-//@Grab('com.gmongo:gmongo:1.0')
+class Example {
+   static void main(String[] args) {
+      Student mst = new Student();
+      mst.name = "Joe";
+      mst.ID = 1;
+      println(mst.Display())
+   } 
+} 
+ 
+public class Student {
+   String name;
+   int ID;
+	
+   String Display() {
+      return name +ID;
+   }  
+}
 
-//import com.gmongo.GMongo
+class StudentTest extends GroovyTestCase {
+   void testDisplay() {
+      def stud = new Student(name : 'Joe', ID : '1')
+      def expected = 'Joe1'
+      assertToString(stud.Display(), expected)
+   }
+}
 
-def gmongo = new GMongo('localhost:27017')
-def db = gmongo.getDB('tips')
+import groovy.util.GroovyTestSuite 
+import junit.framework.Test 
+import junit.textui.TestRunner 
 
-db.so.drop()
-db.so << [name: 'Windows XP']
-db.so << [name: 'Windows 7']
-db.so << [name: 'Windows Vista']
-db.so << [name: 'Mac OS X v10.3 "Panther"']
-db.so << [name: 'Mac OS X v10.4 "Tiger"']
-db.so << [name: 'Mac OS X v10.5 "Leopard"']
-db.so << [name: 'Mac OS X v10.6 "Snow Leopard"']
-db.so << [name: 'Mac OS X v10.7 "Lion"']
+class AllTests { 
+   static Test suite() { 
+      def allTests = new GroovyTestSuite() 
+      allTests.addTestSuite(StudentTest.class) 
+      allTests.addTestSuite(EmployeeTest.class) 
+      return allTests 
+   } 
+} 
 
-// Mac OS Only
-println "\nMac Os Only\n"
-db.so.find([name: ~/^Mac/]).each { so -> println so}
-
-// Print: 
-
-// Mac OS Leopard, Snow Leopard
-println "\nMac OS Leopard, Snow Leopard\n"
-db.so.find([name: ~/Leopard/]).each { so -> println so}
+TestRunner.run(AllTests.suite())
