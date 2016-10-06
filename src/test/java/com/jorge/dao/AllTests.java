7d93315c9924747b5df9189678dfb7aaf7007178
Yorso <jorgeguzon@gmail.com>
2016-10-06 15:03:18 +0200

    changes

--- src/test/java/com/jorge/dao/AllTests.java
+++ src/test/java/com/jorge/dao/AllTests.java
@@ -1,14 +1,14 @@
-import groovy.util.GroovyTestSuite 
-import junit.framework.Test 
-import junit.textui.TestRunner 
+import groovy.util.GroovyTestSuite;
+import junit.framework.Test;
+import junit.textui.TestRunner; 
 
 class AllTests { 
    static Test suite() { 
-      def allTests = new GroovyTestSuite() 
-      allTests.addTestSuite(StudentTest.class) 
-      //allTests.addTestSuite(EmployeeTest.class) 
-      return allTests 
+      def allTests = new GroovyTestSuite(); 
+      allTests.addTestSuite(StudentTest.class); 
+      //allTests.addTestSuite(EmployeeTest.class); 
+      return allTests ;
    } 
 } 
 
-TestRunner.run(AllTests.suite())
+TestRunner.run(AllTests.suite());
--- src/test/java/com/jorge/dao/StudentTest.java
+++ src/test/java/com/jorge/dao/StudentTest.java
@@ -1,9 +1,9 @@
-import com.jorge.model.Student.groovy
+import com.jorge.model.Student;
 
 class StudentTest extends GroovyTestCase {
    void testDisplay() {
-      def stud = new Student(name : 'Joe', ID : '1')
-      def expected = 'Joe1'
-      assertToString(stud.Display(), expected)
+      def stud = new Student(name : 'Joe', ID : '1');
+      def expected = 'Joe1';
+      assertToString(stud.Display(), expected);
    }
 }
