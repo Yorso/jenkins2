8411f58d1e5dc7431cf7b3de9ed9ecb0dcf825cb
Yorso <jorgeguzon@gmail.com>
2016-10-06 15:39:59 +0200

    changes

--- src/test/resources/nosql/mongodb_test.groovy
+++ src/test/resources/nosql/mongodb_test.groovy
@@ -90,7 +90,7 @@ class AllTests {
    static Test suite() { 
       def allTests = new GroovyTestSuite() 
       allTests.addTestSuite(StudentTest.class) 
-      allTests.addTestSuite(EmployeeTest.class) 
+      //allTests.addTestSuite(EmployeeTest.class) 
       return allTests 
    } 
 } 
